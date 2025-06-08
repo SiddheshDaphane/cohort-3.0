04eb70bff23570488be4cd790b20b11e7d6f296f
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-06-07 20:28:21 -0500

    Understanding Synchronous and Asynchronous code - Functional Arguments

--- 2.1 Async JS/index.js
+++ 2.1 Async JS/index.js
@@ -1,4 +1,4 @@
-// This is Synchrinous code
+// This is Synchronous code
 
 const fs = require('fs');
 
@@ -8,3 +8,31 @@ console.log(data);
 const data1 = fs.readFileSync('b.txt', 'utf8');
 console.log(data1);
 
+
+// Functional arguments
+
+// Write a calculator program that adds, subtracts, multiplies, divides two arguments
+
+
+//// Approach 1
+function sum(a,b) {
+  return a+b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function substract(a,b) {
+  return a-b;
+}
+
+function divide(a,b) {
+  return a/b;
+}
+
+function doOperations(a,b,op) {
+  return op(a,b)
+}
+
+console.log(doOperations(2,3,sum))
