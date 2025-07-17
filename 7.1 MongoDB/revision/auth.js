d1ac490d4d45abacde88d0ec43d61a0d14486cfc
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-07-17 18:13:35 -0500

    added comment about token verification payload

--- 7.1 MongoDB/revision/auth.js
+++ 7.1 MongoDB/revision/auth.js
@@ -6,6 +6,9 @@ function auth(req, res, next) {
   const token = req.headers.authorization
   const verifiedToken = jwt.verify(token, JWT_SECRET);
 
+  // ✅ What Will Be in verifiedToken?
+// If the token is valid and signed with the correct JWT_SECRET, then jwt.verify(...) returns the decoded payload — i.e., the original data you passed when the token was created using jwt.sign(...).
+
   if (verifiedToken){
     req.userId = verifiedToken.id;
     next()
