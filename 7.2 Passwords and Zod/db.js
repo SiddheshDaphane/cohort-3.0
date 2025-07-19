7cb2def9b5d86d3e891f961d1e5a7566c505496b
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-07-19 09:17:31 -0500

    modified db.js and added comments to understand the model and schema concept

--- 7.2 Passwords and Zod/db.js
+++ 7.2 Passwords and Zod/db.js
@@ -23,6 +23,10 @@ const TodoModel = mongoose.model('todos', Todo);
 // A Schema defines the structure of your documents — what fields exist, their types, and rules.
 // A Model is like a class with which you interact with the database. It wraps the schema and gives you functions to create, read, update, delete (CRUD) documents.
 
+// So what is 'users'?
+// It tells Mongoose:
+// "Create a model that works with the users collection in MongoDB using the User schema."
+
 // If you don’t create a model using mongoose.model(), then:
 // You can’t interact with the collection in MongoDB.
 // You’re only defining a shape (Schema), but not linking it to any database logic.
