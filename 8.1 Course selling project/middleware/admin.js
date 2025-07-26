383e0100833219f312bc934e555c0aa28c105f65
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-07-26 16:08:01 -0500

    added try-catch block in middlware so that backend won't crash

--- 8.1 Course selling project/middleware/admin.js
+++ 8.1 Course selling project/middleware/admin.js
@@ -3,16 +3,28 @@ const JWT_SECRET_ADMIN = process.env.JWT_SECRET_ADMIN;
 
 function adminMiddleware(req, res, next){
   const token = req.headers.authorization;
-  const verifiedToekn = jwt.verify(token, JWT_SECRET_ADMIN);
 
-  if(verifiedToekn) {
-    req.adminId = verifiedToekn.id;
-    next();
-  } else {
-    res.status(403).json({
-      messgae: "Inavlid token"
-    });
-  }
+  try 
+    {
+      const verifiedToekn = jwt.verify(token, JWT_SECRET_ADMIN);
+    
+      if(verifiedToekn) {
+        req.adminId = verifiedToekn.id;
+        next();
+      }
+      else {
+        return res.status(403).json({
+          success: false,
+          messgae: "Inavlid token"
+        });
+      }
+    } catch(err){
+      return res.status(403).json({
+        success: false,
+        messgae: "Token verification failed",
+        error: err.message
+      })
+    }
 };
 
 
--- 8.1 Course selling project/middleware/user.js
+++ 8.1 Course selling project/middleware/user.js
@@ -4,16 +4,26 @@ const JWT_SECRET_USER = process.env.JWT_SECRET_USER;
 
 function userMiddleware(req, res, next){
   const token = req.headers.authorization;
-  const verifiedToekn = jwt.verify(token, JWT_SECRET_USER);
-  console.log(verifiedToekn);
 
-  if(verifiedToekn) {
-    req.userId = verifiedToekn.id;
-    next();
-  } else {
-    res.status(403).json({
-      messgae: "Inavlid token"
-    });
+  try 
+    {
+      const verifiedToekn = jwt.verify(token, JWT_SECRET_USER);
+    
+      if(verifiedToekn) {
+        req.userId = verifiedToekn.id;
+        next();
+      } else {
+        return res.status(403).json({
+          success: false,
+          messgae: "Inavlid token"
+        });
+      }
+   } catch(err) {
+      return res.status(403).json({
+        success: false,
+        messgae: "Cannot verify token",
+        error: err.messgae
+      })
   }
 };
 
