464ca321b64f9cc9430cb18b38cbd2bec13fb411
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-07-26 14:33:12 -0500

    Signin point of admin

--- 8.1 Course selling project/routes/admin.js
+++ 8.1 Course selling project/routes/admin.js
@@ -80,10 +80,70 @@ adminRouter.post("/signup", async function(req,res) {
 });
 
 
-adminRouter.post("/signin", function(req,res) {
-  res.json({
-    messgae: "Signin endpoint"
+adminRouter.post("/signin", async function(req,res) {
+
+  const requireBody = z.object({
+    email: z.string().email().min(3).max(100),
+    password: z.string()
+      .min(5,"password less than 5 characters")
+      .max(20, "password clonger than 20 characters")
+      .regex(
+        /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/,
+        "Password must contain at least one uppercase letter and one special character"
+      )
   })
+
+  try {
+    const parsedData = requireBody.safeParse(req.body);
+
+    if(!parsedData.success){
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        error: parsedData.error.format()
+      })
+    }
+
+    const { email, password } = req.body;
+
+    const response = await adminModel.findOne({
+      email:email,
+    })
+
+
+    if(!response) {
+      return res.status(401).json({
+        success: false,
+        message: "Inavlid email or password",
+        error: { type: "AuthError", details: "Email not found"}
+      })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, response.password)
+
+    if(!passwordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+        error: { type: "AuthError", details: "Password not found"}
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      messgae: "Signin endpoint",
+      userId: response._id
+    })
+  } catch(err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: {
+        type: "UnhandledError",
+        details: err.message
+      }
+    });
+  }
 });
 
 
